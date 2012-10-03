ff07a322787c81fbbcfe5721e1e0cfae0a8b1cbe
Felix Geisendörfer <felix@debuggable.com>
2012-10-03 14:43:48 +0100

    Update

--- figures/cpp-boundary/benchmark/run.js
+++ figures/cpp-boundary/benchmark/run.js
@@ -5,10 +5,12 @@ var libName   = process.argv[2];
 var lib       = require('./' + libName);
 var loopCount = 50000000;
 
-common.run(libName, loopCount, function() {
+common.run(libName, function(cb) {
   for (var i = 0; i < loopCount; i++) {
     if (lib.twentyThree() !== 23) {
       new Error('failed assert');
     }
   }
+
+  cb(null, {count: loopCount});
 });
